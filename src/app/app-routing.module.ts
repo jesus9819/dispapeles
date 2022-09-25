57bf2c658fba8f61bab6cc5711090092f8f11fb0
jesus9819 <jesuszapata9816@gmail.com>
2022-09-24 19:37:35 -0500

    feat: create view and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RecuperarPassComponent } from './componentes/recuperar-pass/recuperar-pass.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { VerifyUserComponent } from './componentes/verify-user/verify-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'login', pathMatch:'full'},
+  {path: 'login',component:LoginComponent},
+  {path: 'register',component:RegisterComponent},
+  {path: 'verifyUser',component:VerifyUserComponent},
+  {path: 'recuperarPass',component:RecuperarPassComponent},
+  {path: 'dashboard',component:DashboardComponent},
+  {path: '**', redirectTo:'login', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { VerifyUserComponent } from './componentes/verify-user/verify-user.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { RecuperarPassComponent } from './componentes/recuperar-pass/recuperar-pass.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerifyUserComponent,
+    SpinnerComponent,
+    RecuperarPassComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
