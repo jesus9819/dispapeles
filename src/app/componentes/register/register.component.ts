6ec6812e27611f4011be90c578a43a94fc12a017
jesus9819 <jesuszapata9816@gmail.com>
2022-09-24 23:46:54 -0500

    feat: register user at firebase

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +10,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   registrarUsuario: FormGroup;
 
-
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private afAuth: AngularFireAuth) {
     this.registrarUsuario = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      repetirPassword:  ['', [Validators.required, Validators.minLength(6)]],
+      repetirPassword: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   ngOnInit(): void {}
 
   registrar() {
-    const email= this.registrarUsuario.value.email;
-    const password=this.registrarUsuario.value.password;
-    const repetirPassword=this.registrarUsuario.value.repetirPassword;
-
-    console.log(email,password,repetirPassword);
+    const email = this.registrarUsuario.value.email;
+    const password = this.registrarUsuario.value.password;
+    const repetirPassword = this.registrarUsuario.value.repetirPassword;
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // console.log(email, password, repetirPassword);
   }
 }
