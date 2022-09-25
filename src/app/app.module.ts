dbe91ed4e54da30999ffa33ae43faa3ee9c55a37
jesus9819 <jesuszapata9816@gmail.com>
2022-09-24 23:12:54 -0500

    feat: captura de formular register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//modulos
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//componentes
 import { LoginComponent } from './componentes/login/login.component';
 import { RegisterComponent } from './componentes/register/register.component';
 import { VerifyUserComponent } from './componentes/verify-user/verify-user.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { RecuperarPassComponent } from './componentes/recuperar-pass/recuperar-pass.component';
 import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { DashboardComponent } from './componentes/dashboard/dashboard.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registrarUsuario: FormGroup;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registrarUsuario = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repetirPassword:  ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  registrar() {
+    const email= this.registrarUsuario.value.email;
+    const password=this.registrarUsuario.value.password;
+    const repetirPassword=this.registrarUsuario.value.repetirPassword;
+
+    console.log(email,password,repetirPassword);
+  }
 }
